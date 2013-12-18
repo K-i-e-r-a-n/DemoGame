18084340ee1e81250fd4d9f89ff2357bec296997
David <daggad11@gmail.com>
2013-12-18 07:03:06 -0800

    added engine stuff that doesn't work

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -2,10 +2,14 @@
 #define ENGINE_HPP
 #include <vector>
 #include <SFML/Graphics.hpp>
+#include <map>
 
 namespace Engine {
 	std::vector<sf::Vertex> convertToPixels(std::vector<sf::Vertex*> vertices, sf::Vector2f aspectRatio, float conversion, float height);
 	sf::Vertex* getElement(std::vector<sf::Vertex*> vertices, int i);
+	void loadTexture(std::string filename);
+
+	std::map<std::string, sf::Texture> textures;
 }
 
 
