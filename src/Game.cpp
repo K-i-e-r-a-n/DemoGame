33cd67ca339c4fff7131ee01582d3c25d2ac1484
David Garber <daggad95@gmail.com>
2013-12-18 15:45:21 -0800

    fixed conversion

--- src/Game.cpp
+++ src/Game.cpp
@@ -38,9 +38,9 @@ std::vector<sf::Vertex> Game::convertToPixels(std::vector<sf::Vertex*> vertices)
 
 	for (int a  = 0; a < vertices.size(); a++) {
 		copy[a].position.x = copy[a].position.x * aspectRatio.x * conversion;
-		copy[a].position.y = window.size.y - (copy[a].position.y * aspectRatio.y * conversion);
+		copy[a].position.y = window.getSize().y - (copy[a].position.y * aspectRatio.y * conversion);
 		copy[a].texCoords.x = copy[a].position.x * aspectRatio.x * conversion;
-		copy[a].texCoords.y = window.size.y - (copy[a].position.y * aspectRatio.y * conversion);
+		copy[a].texCoords.y = window.getSize().y - (copy[a].position.y * aspectRatio.y * conversion);
 	}
 	return copy;
 }
