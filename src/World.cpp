cc0ecda9618a405077d9934ee4760458efda50a3
Kieran <renoh47@gmail.com>
2013-12-18 07:04:47 -0800

    Fixed tracking of object files

--- src/World.cpp
+++ src/World.cpp
@@ -2,17 +2,17 @@
 
 World::World() {
 	//generate one funky chunky. Don't forget to texture.
-    std::vector<sf::Vertex*> vertices;;
-    vertices.push_back(new sf::Vertex(sf::Vector2f(1, 1), sf::Vector2f(1, 1)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(1, 5), sf::Vector2f(1, 5)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(3, 1), sf::Vector2f(3, 1)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(3, 5), sf::Vector2f(3, 5)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(4, 6), sf::Vector2f(4, 6)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(3, 5), sf::Vector2f(3, 5)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(5, 2), sf::Vector2f(5, 2)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(4, 7), sf::Vector2f(4, 7)));
-    vertices.push_back(new sf::Vertex(sf::Vector2f(6, 1), sf::Vector2f(6, 1)));
-    chunks.push_back(new Chunk(vertices));	
+  std::vector<sf::Vertex*> vertices;;
+  vertices.push_back(new sf::Vertex(sf::Vector2f(1, 1), sf::Vector2f(1, 1)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(1, 5), sf::Vector2f(1, 5)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(3, 1), sf::Vector2f(3, 1)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(3, 5), sf::Vector2f(3, 5)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(4, 6), sf::Vector2f(4, 6)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(3, 5), sf::Vector2f(3, 5)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(5, 2), sf::Vector2f(5, 2)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(4, 7), sf::Vector2f(4, 7)));
+  vertices.push_back(new sf::Vertex(sf::Vector2f(6, 1), sf::Vector2f(6, 1)));
+  chunks.push_back(new Chunk(vertices));	
 }
 
 std::vector<Chunk*> World::getChunks() {
